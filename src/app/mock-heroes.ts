451e1828ee08558ff23c1383ec962d0376c9131e
André Meireles <andre.meireles@maxxcard.com.br>
2021-10-01 16:51:59 -0300

    'state' management in angular

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Doutora Nilce' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombado' },
+    { id: 14, name: 'Compressor' },
+    { id: 15, name: 'Magnolia' },
+    { id: 16, name: 'HomemBorracha' },
+    { id: 17, name: 'Dinamita' },
+    { id: 18, name: 'Dr Ze' },
+    { id: 19, name: 'Foguinho' },
+    { id: 20, name: 'Ventinho' }
+]
